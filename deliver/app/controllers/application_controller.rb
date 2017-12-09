e73b5772490a06858f8ed821d6d649bdb0f36b09
ljnissen <lj_nissen@gmx.de>
2017-12-09 18:30:08 +0000

    Add hello world

--- deliver/app/controllers/application_controller.rb
+++ deliver/app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "hello, world!"
+  	render html: "hello, world! we are happy"
   end
 end
