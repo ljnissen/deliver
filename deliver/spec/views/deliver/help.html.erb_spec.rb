fcbb4a9aed29dfad6460ce0cf88a1e64d8f2042d
ljnissen <lj_nissen@gmx.de>
2017-12-11 19:51:27 +0000

    Add rspec tests for home and help

---
+++ deliver/spec/views/deliver/help.html.erb_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "help page", :type => :request do
+	it "should get help" do
+		get "/static_pages/help"
+		assert_response :success
+	end
+	
+end
---
+++ deliver/spec/views/deliver/home.html.erb_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "home page", :type => :request do
+	it "should get home" do
+		get "/static_pages/home"
+		assert_response :success
+	end
+	
+end
