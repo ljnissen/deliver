463568db1f73b80b1a724098cff9acd98b79f0cf
ljnissen <lj_nissen@gmx.de>
2017-12-15 17:04:03 +0000

    Add custom helpers

--- deliver/spec/views/deliver/home.html.erb_spec.rb
+++ deliver/spec/views/deliver/home.html.erb_spec.rb
@@ -1,12 +1,26 @@
 require "rails_helper"
 
+# spec/support/assert_select_root.rb
+
+module AssertSelectRoot
+  def document_root_element
+    html_document.root
+  end
+end
+
+RSpec.configure do |config|
+    config.include AssertSelectRoot, :type => :request
+end
+
 RSpec.describe "home page", :type => :request do
 	it "should get home" do
 		get "/static_pages/home"
 		assert_response :success
-		
+	end
 
-
+	it "should get home ii" do
+		get "/static_pages/home"
+		assert_select "title", "Deliver"
 	end
 	
 end
