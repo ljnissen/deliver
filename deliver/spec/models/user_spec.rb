9259be1866ae0b15e4561f5078f523eee206311b
ljnissen <lj_nissen@gmx.de>
2018-01-08 17:06:51 +0000

    Add validations and tests thereof to model

--- deliver/spec/models/user_spec.rb
+++ deliver/spec/models/user_spec.rb
@@ -6,4 +6,28 @@ RSpec.describe "model", :type => :request do
 		@user = User.new(name: "Example User", email: "user@example.com")
 		expect(@user).to be_valid
 	end
+
+	it "name should be present" do
+		@user = User.new(name: "Example User", email: "user@example.com")
+		@user.name = "     "
+		expect(@user).not_to be_valid
+	end
+
+	it "email should be present" do
+		@user = User.new(name: "Example User", email: "user@example.com")
+		@user.email = "     "
+		expect(@user).not_to be_valid
+	end
+
+	it "name should not be too long" do
+		@user = User.new(name: "Example User", email: "user@example.com")
+		@user.name = "a" * 51
+		expect(@user).not_to be_valid
+	end
+
+	it "email should not be too long" do
+		@user = User.new(name: "Example User", email: "user@example.com")
+		@user.email = "a" * 244 + "@example.com"
+		expect(@user).not_to be_valid
+	end
 end
