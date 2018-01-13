08e9d7edb208d2484c8e2bbfc0dfc88fb69ad9a5
ljnissen <lj_nissen@gmx.de>
2018-01-13 17:20:08 +0000

    Add debug box

--- deliver/config/routes.rb
+++ deliver/config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'static_pages#contact'
 
+  get '/signup', to: 'users#new'
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
