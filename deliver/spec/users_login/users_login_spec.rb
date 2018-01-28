12f8243d26b45f81c79b9c58e263bb6ea1dea89a
ljnissen <lj_nissen@gmx.de>
2018-01-28 17:55:40 +0000

    Add test for flash message

---
+++ deliver/spec/users_login/users_login_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe "signup_path", :type => :request do
+  it "login with invalid information" do
+    get login_path
+    expect do
+    	assert_template 'sessions/new'
+    	post login_path, session: { email: "", password: "" }
+    	assert_template 'sessions/new'
+    	expect(@flash).not_to be.empty?
+    	get root_path
+    	assert flash.empty?
+    end
+  end
+end
