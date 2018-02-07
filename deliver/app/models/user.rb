599c17c5bf285fce9bdf92941efdb30e05a61ab8
ljnissen <lj_nissen@gmx.de>
2018-02-07 16:44:47 +0000

    Add logout functionality through forget method in model

--- deliver/app/models/user.rb
+++ deliver/app/models/user.rb
@@ -1,23 +1,23 @@
 class User < ActiveRecord::Base
 	attr_accessor :remember_token
-	before_save { email.downcase! }
+	before_save { self.email.downcase! }
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, length: { maximum: 50 },
+	validates :email, presence: true, length: { maximum: 255 },
 							format: { with: VALID_EMAIL_REGEX },
 							uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
 
-	class << self
+	
 		# Returns the hash digest of the given string.
-		def digest(string)
+		def User.digest(string)
 			cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 			BCrypt::Password.create(string, cost: cost)
 		end
 
 		# Returns a random token.
-		def new_token
+		def User.new_token
 			SecureRandom.urlsafe_base64
 		end
 
@@ -31,5 +31,10 @@ class User < ActiveRecord::Base
 		def authenticated?(remember_token)
 			BCrypt::Password.new(remember_digest).is_password?(remember_token)
 		end
-	end
+
+		# Forgets a user
+		def forget
+			update_attribute(:remember_digest, nil)
+		end
+	
 end
