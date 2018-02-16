e2b29c0f11d89c3250921dcc5d59f1463ddc2a40
ljnissen <lj_nissen@gmx.de>
2018-02-16 16:19:47 +0000

    Handle unsuccessful edits

--- deliver/app/controllers/users_controller.rb
+++ deliver/app/controllers/users_controller.rb
@@ -25,6 +25,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a succesful update.
+    else
+      render 'edit'
+    end
+  end
+
   private
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
