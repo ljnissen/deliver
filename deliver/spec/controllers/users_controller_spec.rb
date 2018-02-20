f7b4396f7e596081e602bbede1a64ed128cc86cb
ljnissen <lj_nissen@gmx.de>
2018-02-20 17:11:19 +0000

    Required right user

--- deliver/spec/controllers/users_controller_spec.rb
+++ deliver/spec/controllers/users_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 require 'spec_helper'
 
+def setup
+	@user 		= users(:lj)
+	@other_user = users(:archer)
+end
+
 RSpec.describe UsersController, type: :controller do
 
   describe "GET  users new" do
--- deliver/spec/users_edit/users_edit_spec.rb
+++ deliver/spec/users_edit/users_edit_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 #require 'spec_helper'
 
 		def setup
-			@user = users :lj 
+			@user = users(:lj) 
 		end
 
 RSpec.describe "Invalid edits", type: :request do	
+	fixtures :users
 
 	it "unsuccessful edit" do
+		#log_in_as(@user)
 
-		# chapter 10.1.3
+		# chapter 10.1.3 :id => nil, are fixtures loaded?
 		get edit_user_path(@user)
 		#assert_template 'users/edit'
 		#patch user_path(@user), user: {  name: "",
