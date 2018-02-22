7cad6ded55d0134f1fe61320c44b55f9044af67e
ljnissen <lj_nissen@gmx.de>
2018-02-22 16:56:38 +0000

    Add index action to users to display all

--- deliver/spec/controllers/users_controller_spec.rb
+++ deliver/spec/controllers/users_controller_spec.rb
@@ -9,6 +9,11 @@ end
 RSpec.describe UsersController, type: :controller do
 
   describe "GET  users new" do
+  	it "should redirect index when not logged in" do
+  		get users_path
+  		assert_redirect_to login_url
+  	end
+
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
