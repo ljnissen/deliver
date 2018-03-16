4ccdff4138f76e417e8e6c8b59f2da1ac95cf7bd
ljnissen <lj_nissen@gmx.de>
2018-03-16 17:09:11 +0000

    Add password reset mailer and templates

--- deliver/app/controllers/password_resets_controller.rb
+++ deliver/app/controllers/password_resets_controller.rb
@@ -8,7 +8,7 @@ class PasswordResetsController < ApplicationController
   	if @user
   		@user.create_reset_digest
   		@user.send_password_reset_email
-  		flash[:info] = "Email send with password reset instructions"
+  		flash[:info] = "Email sent with password reset instructions"
   		redirect_to root_url
   	else
   		flash.now[:danger] = "Email address not found"
