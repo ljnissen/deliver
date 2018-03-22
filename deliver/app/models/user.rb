3229a07330cee56a4dbfe310adeabec15757bc53
ljnissen <lj_nissen@gmx.de>
2018-03-22 17:19:15 +0000

    Update the reset

--- deliver/app/models/user.rb
+++ deliver/app/models/user.rb
@@ -64,6 +64,11 @@ class User < ActiveRecord::Base
 			UserMailer.password_reset(self).deliver_now
 		end
 
+		# Returns true if a password reset has expired.
+		def password_reset_expired?
+			reset_sent_at < 3.hours.ago
+		end
+
 		private
 
 		# Converts email to all lower-case.
